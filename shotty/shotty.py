5917493ea11b11d8934439af82736309b3f5bd25
Glen Justus <glendjustus@gmail.com>
2020-04-17 16:59:04 -0500

    Handle error from AWS when stopping a pending instance or starting a stopping instance.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -33,10 +34,21 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
+
         i.stop()
+        i.wait_until_stopped()
+
         for v i.volumes.all():
-            print("Creating snapshot of {0}".format(v.id))
+            print("   Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAnalyzer 3000")
+
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.waiting_until_running()
+
+    print("The job is done running!")
 
     return
 
@@ -122,7 +134,11 @@ def list_instances(project):
 
      for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0}. ".format(i.id) + str(e))
+            continue
 
      return
 
@@ -136,7 +152,11 @@ def list_instances(project):
 
      for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0}. ".format(i.id) + str(e))
+            continue
 
      return
 
